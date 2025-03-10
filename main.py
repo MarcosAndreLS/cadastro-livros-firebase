f2b6633dd30ed835225cd1104c57fb25af1f8ba8
MarcosAndreLS <marcosofc04@gmail.com>
2025-03-10 20:34:48 -0300

    telas pyqt5 funcionando, falta linkar botoes

--- main.py
+++ main.py
@@ -1,2 +1,44 @@
+import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplicationfrom src.gui.main_window import MainWindow
+from PyQt5.QtWidgets import QMainWindow
+from src.gui.main_window import Main_window
+from src.gui.login_window import Login_window
+from src.gui.sign_up_window import Sign_up_window
+
+class Ui_Main(QtWidgets.QWidget):
+    """
+    Classe Ui_Main responsável pela configuração da interface gráfica.
+    """
+
+    def setupUi(self, Main):
+        Main.setObjectName('Main')
+        Main.resize(640, 480)
+
+        self.QtStack = QtWidgets.QStackedLayout()
+
+        self.stack0 = QtWidgets.QMainWindow()
+        self.stack1 = QtWidgets.QMainWindow()
+        self.stack2 = QtWidgets.QMainWindow()
+
+        self.mainWindow = Main_window()
+        self.mainWindow.setupUi(self.stack0)
+
+        self.loginWindow = Login_window()
+        self.loginWindow.setupUi(self.stack1)
+
+        self.signupWindow = Sign_up_window()
+        self.signupWindow.setupUi(self.stack2)
+
+        self.QtStack.addWidget(self.stack0)
+        self.QtStack.addWidget(self.stack1)
+        self.QtStack.addWidget(self.stack2)
+
+class Main(QMainWindow, Ui_Main):
+    def __init__(self):
+        super(Main, self).__init__(None)
+        self.setupUi(self)
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    show_main = Main()
+    sys.exit(app.exec_())
