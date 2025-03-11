7c3a03bb65daea4e67ed3129ff2ec937156c410d
MarcosAndreLS <marcosofc04@gmail.com>
2025-03-10 22:07:52 -0300

    linkado com firebase e realtime data base

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from PyQt5.QtWidgets import QMainWindow
 from src.gui.main_window import Main_window
 from src.gui.login_window import Login_window
 from src.gui.sign_up_window import Sign_up_window
+from src.database.firebase_config import auth, db
+
 
 class Ui_Main(QtWidgets.QWidget):
     """
@@ -37,6 +39,63 @@ class Main(QMainWindow, Ui_Main):
     def __init__(self):
         super(Main, self).__init__(None)
         self.setupUi(self)
+
+        self.mainWindow.pushButton.clicked.connect(self.abrirLogin)
+        self.mainWindow.pushButton_2.clicked.connect(self.abrirCadastro)
+        self.loginWindow.pushButton.clicked.connect(self.voltar_main_window)
+        self.signupWindow.pushButton_3.clicked.connect(self.voltar_main_window)
+        self.loginWindow.pushButton_2.clicked.connect(self.loginfunction)
+        self.signupWindow.pushButton_4.clicked.connect(self.createaccfunction)
+
+    
+    def abrirLogin(self):
+        self.QtStack.setCurrentIndex(2)
+    
+    def abrirCadastro(self):
+        self.QtStack.setCurrentIndex(1)
+    
+    def voltar_main_window(self):
+        self.QtStack.setCurrentIndex(0)
+    
+    def loginfunction(self):
+        email = self.loginWindow.lineEdit.text()
+        password = self.loginWindow.lineEdit_2.text()
+
+        try:
+            users = db.child("users").get().val()  # Obtém todos os usuários cadastrados
+
+            if users:
+                for user_id, user_data in users.items():
+                    if user_data['email'] == email and user_data['password'] == password:
+                        print(f"Login bem-sucedido! Bem-vindo, {email}")
+                        self.QtStack.setCurrentIndex(0)  # Redireciona para a tela principal
+                        return
+
+            print("Erro: Usuário ou senha incorretos!")
+
+        except Exception as e:
+            print(f"Erro no login: {str(e)}")
+
+    def createaccfunction(self):
+        email = self.signupWindow.lineEdit_5.text()
+        password = self.signupWindow.lineEdit_6.text()
+        confirm_password = self.signupWindow.lineEdit_4.text()
+
+        if password == confirm_password:
+            try:
+                # Criando um ID único para o usuário
+                new_user = db.child("users").push({
+                    "email": email,
+                    "password": password  # OBS: Senha armazenada sem hash (não recomendado para produção)
+                })
+
+                print("Conta criada com sucesso!")
+                self.QtStack.setCurrentIndex(0)  # Redireciona para a tela principal
+
+            except Exception as e:
+                print(f"Erro ao criar conta: {str(e)}")
+        else:
+            print("Erro: As senhas não coincidem!")
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
--- src/gui/login_window.py
+++ src/gui/login_window.py
@@ -12,12 +12,14 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 
 class Login_window(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(752, 559)
-        self.verticalLayout = QtWidgets.QVBoxLayout(Dialog)
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName("verticalLayout")
-        self.textBrowser = QtWidgets.QTextBrowser(Dialog)
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
         self.textBrowser.setMinimumSize(QtCore.QSize(0, 0))
         self.textBrowser.setLayoutDirection(QtCore.Qt.LeftToRight)
         self.textBrowser.setStyleSheet("background: transparent;\n"
@@ -26,7 +28,7 @@ class Login_window(object):
         self.verticalLayout.addWidget(self.textBrowser)
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.label = QtWidgets.QLabel(Dialog)
+        self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setMinimumSize(QtCore.QSize(300, 50))
         self.label.setMaximumSize(QtCore.QSize(300, 50))
         font = QtGui.QFont()
@@ -34,7 +36,7 @@ class Login_window(object):
         self.label.setFont(font)
         self.label.setObjectName("label")
         self.horizontalLayout.addWidget(self.label)
-        self.lineEdit = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.lineEdit.setMinimumSize(QtCore.QSize(300, 50))
         self.lineEdit.setMaximumSize(QtCore.QSize(300, 50))
         self.lineEdit.setStyleSheet("background: transparent;\n"
@@ -44,7 +46,7 @@ class Login_window(object):
         self.verticalLayout.addLayout(self.horizontalLayout)
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setMinimumSize(QtCore.QSize(300, 50))
         self.label_2.setMaximumSize(QtCore.QSize(300, 50))
         font = QtGui.QFont()
@@ -52,7 +54,7 @@ class Login_window(object):
         self.label_2.setFont(font)
         self.label_2.setObjectName("label_2")
         self.horizontalLayout_2.addWidget(self.label_2)
-        self.lineEdit_2 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
         self.lineEdit_2.setMinimumSize(QtCore.QSize(300, 50))
         self.lineEdit_2.setMaximumSize(QtCore.QSize(300, 50))
         self.lineEdit_2.setStyleSheet("background: transparent;\n"
@@ -62,7 +64,7 @@ class Login_window(object):
         self.verticalLayout.addLayout(self.horizontalLayout_2)
         self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_3.setObjectName("horizontalLayout_3")
-        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -74,35 +76,43 @@ class Login_window(object):
         self.horizontalLayout_3.addWidget(self.pushButton)
         spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout_3.addItem(spacerItem)
-        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_2.setMinimumSize(QtCore.QSize(100, 50))
         self.pushButton_2.setMaximumSize(QtCore.QSize(100, 50))
         self.pushButton_2.setObjectName("pushButton_2")
         self.horizontalLayout_3.addWidget(self.pushButton_2)
         self.verticalLayout.addLayout(self.horizontalLayout_3)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, Dialog):
+    def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.textBrowser.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.textBrowser.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
 "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:28pt; color:#55007f;\">Login</span></p></body></html>"))
-        self.label.setText(_translate("Dialog", "E-mail:"))
-        self.label_2.setText(_translate("Dialog", "Senha:"))
-        self.pushButton.setText(_translate("Dialog", "Voltar"))
-        self.pushButton_2.setText(_translate("Dialog", "Logar"))
+        self.label.setText(_translate("MainWindow", "E-mail:"))
+        self.label_2.setText(_translate("MainWindow", "Senha:"))
+        self.pushButton.setText(_translate("MainWindow", "Voltar"))
+        self.pushButton_2.setText(_translate("MainWindow", "Logar"))
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec_())
